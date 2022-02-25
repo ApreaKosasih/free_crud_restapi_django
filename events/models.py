f0a4a799c93979c61e19309cac53359cefa290d3
Aprea <apreak@gmail.com>
2022-02-25 16:40:47 +0700

    update auto pk

--- events/models.py
+++ events/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
+    id = models.AutoField(primary_key=True)
     class JenisKelamin(models.TextChoices):
         LAKI_LAKI = 'L'
         PEREMPUAN = 'P'
