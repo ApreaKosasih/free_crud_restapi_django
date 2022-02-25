21124c3f2803e4a8156db827da99020f1bca24c7
Aprea <apreak@gmail.com>
2022-02-25 14:43:41 +0700

    simple crud profile

---
+++ free_crud_restapi_django/events/models.py
@@ -0,0 +1,25 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    class JenisKelamin(models.TextChoices):
+        LAKI_LAKI = 'L'
+        PEREMPUAN = 'P'
+    jenis_kelamin = models.CharField(
+        max_length=1,
+        choices=JenisKelamin.choices,
+        default=JenisKelamin.LAKI_LAKI,
+    )
+    nama = models.CharField(max_length=50)
+    alamat = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=6)
+    other = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'events'
+
+    def __str__(self):
+        return self.nama
